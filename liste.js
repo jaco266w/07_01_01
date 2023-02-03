dd3d7f6dd12bf9853f4eee3b87585adac501a689
Jacob Joergensen <jacobjoergensen23@gmail.com>
2023-02-03 11:50:09 +0100

    Dynamisk produkt liste

---
+++ liste.js
@@ -0,0 +1,34 @@
+console.log("Test af konsol");
+
+const url = `https://kea-alt-del.dk/t7/api/products?limit=100`;
+
+async function getData(url) {
+    const resultat = await fetch(url);
+    const liste = await resultat.json();
+
+    vis(liste);
+}
+
+function vis(liste) {
+    console.log(liste);
+
+    const beholder = document.querySelector("#product_list_grid");
+    const skabelon = document.querySelector("#liste_skabelon").content;
+
+    liste.forEach(produkt => {
+        const klon = skabelon.cloneNode(true);
+
+        klon.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${produkt.id}.webp`;
+        klon.querySelector("img").alt = produkt.productdisplayname;
+        klon.querySelector("h3").textContent = produkt.productdisplayname;
+        klon.querySelector(".price").textContent = produkt.price + ",-" + "DKK";
+
+        klon.querySelector(".discounted .rabat").textContent = produkt.discount + "%";
+        klon.querySelector(".discounted .ny_pris").textContent = "Now" + Math.floor(produkt.price*(1-(produkt.discount/100))) + ",-" + "DKK";
+
+        beholder.appendChild(klon);
+    });
+
+}
+
+getData(url);
